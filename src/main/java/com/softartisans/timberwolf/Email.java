dba219ea672c933b2a4d8958c899c432088c5aaa
Sean Kermes <seank@softartisans.com>
2011-12-06 10:34:14 -0500

    Adding more common headers.

--- src/main/java/com/softartisans/timberwolf/Email.java
+++ src/main/java/com/softartisans/timberwolf/Email.java
@@ -4,10 +4,12 @@ import java.util.Map;
 
 public interface Email {
     String getBody();
+    String getSender();
     String[] getRecipients();
     String[] getCcRecipients();
     String[] getBccRecipients();
     String getSubject();
+    DateTime getTimeSent();
     // TODO: What other common headers do we want?
 
     String getHeader(String header);
