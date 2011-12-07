d08f75b3b6a039b30d23723c470f4940ac85490e
Sean Kermes <seank@softartisans.com>
2011-12-07 18:15:45 -0500

    Fixing checkstyle errors.

--- src/main/java/com/softartisans/timberwolf/ExchangeEmail.java
+++ src/main/java/com/softartisans/timberwolf/ExchangeEmail.java
@@ -19,7 +19,8 @@ import java.util.Map;
  * <li>"Sender": The email address of the user who sent the email.
  * </ul>
  */
-public class ExchangeEmail implements MailboxItem {
+public class ExchangeEmail implements MailboxItem
+{
     private static final String BODY_KEY = "Body";
     private static final String SUBJECT_KEY = "Subject";
     private static final String TIME_SENT_KEY = "Time Sent";
@@ -29,43 +30,53 @@ public class ExchangeEmail implements MailboxItem {
     /** The headers that this email exports. */
     private Map<String, String> headers;
 
-    public ExchangeEmail(final MessageType message) {
+    public ExchangeEmail(final MessageType message)
+    {
         headers = new HashMap<String, String>();
 
-        if (message.isSetBody()) {
+        if (message.isSetBody())
+        {
             headers.put(BODY_KEY, message.getBody().getStringValue());
         }
 
-        if (message.isSetSubject()) {
+        if (message.isSetSubject())
+        {
             headers.put(SUBJECT_KEY, message.getSubject());
         }
 
-        if (message.isSetDateTimeSent()) {
+        if (message.isSetDateTimeSent())
+        {
             String time = message.getDateTimeSent().getTime().toString();
             headers.put(TIME_SENT_KEY, time);
         }
 
-        if (message.isSetItemId()) {
+        if (message.isSetItemId())
+        {
             headers.put(ID_KEY, message.getItemId().getId());
         }
 
-        if (message.isSetFrom()) {
+        if (message.isSetFrom())
+        {
             EmailAddressType address = message.getFrom().getMailbox();
-            if (address.isSetEmailAddress()) {
+            if (address.isSetEmailAddress())
+            {
                 headers.put(SENDER_KEY, address.getEmailAddress());
             }
         }
     }
 
-    public final String[] getHeaderKeys() {
+    public final String[] getHeaderKeys()
+    {
         return headers.keySet().toArray(new String[0]);
     }
 
-    public final boolean hasKey(final String key) {
+    public final boolean hasKey(final String key)
+    {
         return headers.containsKey(key);
     }
 
-    public final String getHeader(final String key) {
+    public final String getHeader(final String key)
+    {
         return headers.get(key);
     }
 }
