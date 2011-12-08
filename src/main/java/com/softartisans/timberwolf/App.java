5726ccd71931be8fdfa522ac2d091ec16e19e326
Sean Kermes <seank@softartisans.com>
2011-12-08 14:45:31 -0500

    Removing auto-genned cruft from App.java.

--- src/main/java/com/softartisans/timberwolf/App.java
+++ src/main/java/com/softartisans/timberwolf/App.java
@@ -1,21 +1,12 @@
-/*
- * Copyright (c) 2011 Softartisans
- */
 package com.softartisans.timberwolf;
 
-/**
- * Hello world!
- */
-final class App {
-    /** Private because this class should not be instantiated. */
-    private App() {
+final class App
+{
+    private App()
+    {
     }
 
-    /**
-     * This is the main function.
-     * @param args this takes nothing for now
-     */
-    public static void main(final String[] args) {
-        System.out.println("Hello World!");
+    public static void main(final String[] args)
+    {
     }
 }
