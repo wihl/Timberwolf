5895b484898c3cd261ddd45f550b59294491b8b5
Sean Kermes <seank@softartisans.com>
2011-12-12 15:49:29 -0500

    First draft of making a request.

--- src/main/java/com/softartisans/timberwolf/ExchangeMailStore.java
+++ src/main/java/com/softartisans/timberwolf/ExchangeMailStore.java
@@ -1,6 +1,16 @@
 package com.softartisans.timberwolf;
 
+import java.io.IOException;
+
 import java.util.Iterator;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
 /**
  * ExchangeMailStore represents a remote Exchange mail store.  It uses the
@@ -24,11 +34,39 @@ public class ExchangeMailStore implements MailStore
      * ExchangeMailStore will connect to.
      */
     public ExchangeMailStore(String user, String password, String endpoint)
-    {        
+    {
+        this.user = user;
+        this.password = password;
     }
 
     public Iterator<MailboxItem> getMail(String user)
-    {        
+    {
+        DefaultHttpClient client = new DefaultHttpClient();
+        try
+        {
+            client.getCredentialsProvider().setCredentials(
+                new AuthScope("localhost", 443),
+                new UsernamePasswordCredentials(this.user, password));
+            HttpGet get = new HttpGet("https://devexch01.int.tartarus.com/ews/exchange.asmx");
+            System.out.println("executing request" + get.getRequestLine());
+            HttpResponse response = client.execute(get);
+            HttpEntity entity = response.getEntity();
+            System.out.println("-------------------------------------");
+            System.out.println(response.getStatusLine());
+            if (entity != null)
+            {
+                System.out.println("Response content length: " + entity.getContentLength());
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println("IO Exception: " + e.getMessage());
+        }
+        finally
+        {
+            client.getConnectionManager().shutdown();
+        }
+
         return null;
     }
 }
