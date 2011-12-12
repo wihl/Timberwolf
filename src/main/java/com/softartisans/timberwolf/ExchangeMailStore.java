426bbae02a9fb76fef2c893cd0646aa506d0674c
Sean Kermes <seank@softartisans.com>
2011-12-12 14:46:22 -0500

    Removing all the axis2 code from ExchangeMailStore.

--- src/main/java/com/softartisans/timberwolf/ExchangeMailStore.java
+++ src/main/java/com/softartisans/timberwolf/ExchangeMailStore.java
@@ -1,21 +1,6 @@
 package com.softartisans.timberwolf;
 
-import com.microsoft.schemas.exchange.services._2006.messages.FindItemDocument;
-import com.microsoft.schemas.exchange.services._2006.messages.FindItemResponseDocument;
-import com.microsoft.schemas.exchange.services._2006.messages.FindItemType;
-import com.microsoft.schemas.exchange.services._2006.types.ItemResponseShapeType;
-import com.microsoft.schemas.exchange.services._2006.types.DefaultShapeNamesType;
-import com.microsoft.schemas.exchange.services._2006.types.NonEmptyArrayOfBaseFolderIdsType;
-import com.microsoft.schemas.exchange.services._2006.types.DistinguishedFolderIdType;
-import com.microsoft.schemas.exchange.services._2006.types.DistinguishedFolderIdNameType;
-
-import com.softartisans.timberwolf.exchangeservice.ExchangeServiceStub;
-
 import java.util.Iterator;
-
-import java.rmi.RemoteException;
-
-import org.apache.axis2.AxisFault;
 
 /**
  * ExchangeMailStore represents a remote Exchange mail store.  It uses the
@@ -23,7 +8,6 @@ import org.apache.axis2.AxisFault;
  */
 public class ExchangeMailStore implements MailStore
 {
-    ExchangeServiceStub stub;
     private String user;
     private String password;
 
@@ -40,45 +24,11 @@ public class ExchangeMailStore implements MailStore
      * ExchangeMailStore will connect to.
      */
     public ExchangeMailStore(String user, String password, String endpoint)
-    {
-        try
-        {         
-            stub = new ExchangeServiceStub(endpoint);
-            this.user = user;
-            this.password = password;   
-        }        
-        catch (AxisFault e)
-        {
-            System.out.println(e.getMessage());
-        }
+    {        
     }
 
     public Iterator<MailboxItem> getMail(String user)
-    {
-        try
-        {
-            FindItemDocument request = FindItemDocument.Factory.newInstance();
-            FindItemType find = request.addNewFindItem();
-            ItemResponseShapeType itemShape = find.addNewItemShape();
-            itemShape.setBaseShape(DefaultShapeNamesType.ALL_PROPERTIES);
-            NonEmptyArrayOfBaseFolderIdsType folderIds = 
-                find.addNewParentFolderIds();
-            DistinguishedFolderIdType parentFolder = 
-                folderIds.addNewDistinguishedFolderId();
-            parentFolder.setId(DistinguishedFolderIdNameType.INBOX);
-
-            FindItemResponseDocument response = stub.findItem(request, null, null, 
-                                                            null, null);
-            System.out.println(response.getFindItemResponse()
-                                       .getResponseMessages()
-                                       .getFindItemResponseMessageArray()
-                                       .length);            
-        }        
-        catch (RemoteException e)
-        {
-            System.out.println(e.getMessage());
-        }
-
+    {        
         return null;
     }
 }
