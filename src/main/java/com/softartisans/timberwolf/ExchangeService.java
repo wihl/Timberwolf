45220753c8f77a11dc09e155c71df64580bb1f74
Scott Dugas <scottd@softartisans.com>
2011-12-15 10:22:25 -0500

    Partially implemented calling FindItems
    Still needs to actually parse the response, but I'm waiting for
    XmlBeans to do that

--- src/main/java/com/softartisans/timberwolf/ExchangeService.java
+++ src/main/java/com/softartisans/timberwolf/ExchangeService.java
@@ -2,6 +2,8 @@ package com.softartisans.timberwolf;
 
 import com.cloudera.alfredo.client.AuthenticatedURL;
 import com.cloudera.alfredo.client.AuthenticationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,12 +13,15 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Iterator;
+import java.util.Vector;
 
 /**
  * This is the MailStore implementation for Exchange email
  */
 public class ExchangeService implements MailStore
 {
+    private static final Logger log = LoggerFactory.getLogger(
+            ExchangeService.class);
     /**
      * When FindItems is run, you can limit the number of items to get at a time
      * and page, starting with 1000, but we'll probably want to profile this a
@@ -52,6 +57,14 @@ public class ExchangeService implements MailStore
         this.exchangeUrl = exchangeUrl;
     }
 
+    private static byte[] getFindItemsRequest(int offset, String folder)
+            throws UnsupportedEncodingException
+    {
+        // TODO paging,and ask folder insertion
+        String fi = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\"\n               xmlns:t=\"http://schemas.microsoft.com/exchange/services/2006/types\">\n    <soap:Body>\n        <FindItem xmlns=\"http://schemas.microsoft.com/exchange/services/2006/messages\"\n                  xmlns:t=\"http://schemas.microsoft.com/exchange/services/2006/types\"\n                  Traversal=\"Shallow\">\n            <ItemShape>\n                <t:BaseShape>IdOnly</t:BaseShape>\n            </ItemShape>\n            <ParentFolderIds>\n                <t:DistinguishedFolderId Id=\"inbox\"/>\n            </ParentFolderIds>\n        </FindItem>\n    </soap:Body>\n</soap:Envelope>";
+        return fi.getBytes("UTF-8");
+    }
+
     @Override
     public Iterator<MailboxItem> getMail(String user)
             throws IOException, AuthenticationException
@@ -61,15 +74,74 @@ public class ExchangeService implements MailStore
 
     private static class EmailIterator implements Iterator<MailboxItem>
     {
+        Vector<String> currentIds;
+        int currentIdIndex = 0;
+        int findItemsOffset = 0;
+        String exchangeUrl;
 
-        private EmailIterator()
+        private EmailIterator(String exchangeUrl)
         {
+            this.exchangeUrl = exchangeUrl;
+        }
+
+        private static Vector<String> findItems(int offset, String exchangeUrl)
+                throws IOException, AuthenticationException
+        {
+            AuthenticatedURL.Token token = new AuthenticatedURL.Token();
+            URL url = new URL(exchangeUrl);
+            HttpURLConnection conn = new AuthenticatedURL().openConnection(url,
+                                                                           token);
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+            conn.setReadTimeout(10000);
+            conn.setRequestProperty("Content-Type", "text/xml");
+            byte[] bytes = getFindItemsRequest(offset, "inbox");
+            conn.setRequestProperty("Content-Length", "" + bytes.length);
+            conn.getOutputStream().write(bytes);
+
+            if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String line = reader.readLine();
+                while (line != null) {
+                    System.out.println(line);
+                    line = reader.readLine();
+                }
+                reader.close();
+                // TODO: parse response
+                return new Vector<String>();
+            }
+            else
+            {
+                log.error("Failed to find items; Status code: "
+                          + conn.getResponseCode() + " "
+                          + conn.getResponseMessage());
+            }
+
+            return new Vector<String>();
         }
 
         @Override
         public boolean hasNext()
         {
-            return false;
+            if (currentIds == null)
+            {
+                try
+                {
+                    currentIdIndex = 0;
+                    currentIds = findItems(findItemsOffset, exchangeUrl);
+                }
+                catch (IOException e)
+                {
+                    log.error("findItems failed to get ids", e);
+                    return false;
+                }
+                catch (AuthenticationException e)
+                {
+                    log.error("findItems could not authenticate", e);
+                    return false;
+                }
+            }
+            return currentIdIndex < currentIds.size();
         }
 
         @Override
