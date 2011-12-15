7cf0496aff42371d47d242c1bf604867a09592d7
George Schneeloch <georges@softartisans.com>
2011-12-15 15:26:46 -0500

    i got kerberos working with cxf\!

--- src/main/java/com/softartisans/timberwolf/App.java
+++ src/main/java/com/softartisans/timberwolf/App.java
@@ -1,11 +1,56 @@
 package com.softartisans.timberwolf;
 
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+import javax.xml.namespace.QName;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.Holder;
+
+import org.apache.cxf.binding.soap.SoapMessage;
+import org.apache.cxf.binding.soap.interceptor.AbstractSoapInterceptor;
+import org.apache.cxf.configuration.security.AuthorizationPolicy;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.frontend.ClientProxy;
+import org.apache.cxf.headers.Header;
+import org.apache.cxf.interceptor.Fault;
+import org.apache.cxf.interceptor.Interceptor;
+import org.apache.cxf.interceptor.LoggingInInterceptor;
+import org.apache.cxf.interceptor.LoggingOutInterceptor;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.apache.cxf.jaxws.handler.soap.SOAPHandlerInterceptor;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.phase.AbstractPhaseInterceptor;
+import org.apache.cxf.phase.Phase;
+import org.apache.cxf.transport.http.HTTPConduit;
+import org.apache.cxf.transports.http.configuration.HTTPClientPolicy;
+import org.junit.runners.ParentRunner;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.microsoft.schemas.exchange.services._2006.messages.ExchangeService;
+import com.microsoft.schemas.exchange.services._2006.messages.ExchangeServicePortType;
+import com.microsoft.schemas.exchange.services._2006.messages.FindItemResponseType;
+import com.microsoft.schemas.exchange.services._2006.messages.FindItemType;
+import com.microsoft.schemas.exchange.services._2006.types.BaseFolderIdType;
+import com.microsoft.schemas.exchange.services._2006.types.DefaultShapeNamesType;
+import com.microsoft.schemas.exchange.services._2006.types.DistinguishedFolderIdNameType;
+import com.microsoft.schemas.exchange.services._2006.types.DistinguishedFolderIdType;
+import com.microsoft.schemas.exchange.services._2006.types.ItemQueryTraversalType;
+import com.microsoft.schemas.exchange.services._2006.types.ItemResponseShapeType;
+import com.microsoft.schemas.exchange.services._2006.types.NonEmptyArrayOfBaseFolderIdsType;
+import com.microsoft.schemas.exchange.services._2006.types.ServerVersionInfo;
 
 /**
  * Driver class to grab emails and put them in HBase.
@@ -55,7 +100,8 @@ final class App
 
     public static void main(final String[] args)
     {
-        new App().run(args);
+        new App().run(new String[]{"--get-email-for", "developer", "--exchange-url", "https://devexch01.int.tartarus.com/ews/Exchange.asmx",
+                "--exchange-user", "developer", "--exchange-password", "pass@word1"});
     }
 
     private void run(final String[] args)
@@ -77,6 +123,38 @@ final class App
             log.info("HBase Table Name: {}", hbaseTableName);
             log.info("HBase Column Family: {}", hbaseColumnFamily);
 
+            ExchangeService service = new ExchangeService(new URL("file:///home/georges/timberwolf/src/main/resources/wsdl/Services.wsdl"));
+            
+            ExchangeServicePortType port = service.getExchangePort();
+            Client client = ClientProxy.getClient(port);
+            client.getInInterceptors().add(new LoggingInInterceptor());
+            client.getOutInterceptors().add(new LoggingOutInterceptor());
+
+            
+            HTTPConduit conduit = (HTTPConduit)client.getConduit();
+            HTTPClientPolicy httpClientPolicy = new HTTPClientPolicy();
+            httpClientPolicy.setAllowChunking(false);
+            conduit.setClient(httpClientPolicy);
+            
+            AuthorizationPolicy auth = new AuthorizationPolicy();
+            auth.setUserName("bkerr");
+            //auth.setPassword("pass@word1");
+            conduit.setAuthorization(auth);
+            
+            FindItemType type = new FindItemType();
+            type.setTraversal(ItemQueryTraversalType.SHALLOW);
+            ItemResponseShapeType shapeType = new ItemResponseShapeType();
+            shapeType.setBaseShape(DefaultShapeNamesType.ID_ONLY);
+            type.setItemShape(shapeType);
+            
+            NonEmptyArrayOfBaseFolderIdsType folders = new NonEmptyArrayOfBaseFolderIdsType();
+            DistinguishedFolderIdType distinguishedFolderIdType = new DistinguishedFolderIdType();
+            distinguishedFolderIdType.setId(DistinguishedFolderIdNameType.INBOX);
+            folders.getFolderIdOrDistinguishedFolderId().add(distinguishedFolderIdType);
+            type.setParentFolderIds(folders);
+            
+            port.findItem(type, null, null, null, null, null, null);
+            
             boolean noHBaseArgs =
                     hbaseQuorum == null && hbasePort == null
                     && hbaseTableName == null;
@@ -101,7 +179,11 @@ final class App
             parser.printUsage(System.err);
             System.err.println();
             return;
+        } catch (MalformedURLException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
-
+        
     }
 }
