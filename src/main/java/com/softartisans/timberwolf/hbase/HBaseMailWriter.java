5214af6aa62d35e2051204f93531d18aaf8db7de
Nick Martin <nickm@softartisans.com>
2011-12-16 10:39:34 -0800

    Updated comment.

--- src/main/java/com/softartisans/timberwolf/hbase/HBaseMailWriter.java
+++ src/main/java/com/softartisans/timberwolf/hbase/HBaseMailWriter.java
@@ -2,14 +2,13 @@ package com.softartisans.timberwolf.hbase;
 
 import com.softartisans.timberwolf.MailWriter;
 import com.softartisans.timberwolf.MailboxItem;
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * This class writes a list of MailboxItems to an HTableInterface.
+ * This class writes a list of MailboxItems to an IHBaseTable.
  */
 public class HBaseMailWriter implements MailWriter
 {
