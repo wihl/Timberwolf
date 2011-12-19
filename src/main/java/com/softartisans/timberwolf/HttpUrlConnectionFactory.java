ddc434cecf13e5c1dbd86c779bcbcba1211bbc98
Sean Kermes <seank@softartisans.com>
2011-12-19 17:46:56 -0500

    Javadoc-ing the factory interface.

--- src/main/java/com/softartisans/timberwolf/HttpUrlConnectionFactory.java
+++ src/main/java/com/softartisans/timberwolf/HttpUrlConnectionFactory.java
@@ -8,8 +8,13 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 
+/** Represents a factory for HTTP requests. */
 interface HttpUrlConnectionFactory
 {
+    /** 
+     * Constructs a new HttpURLConnection that will make a request to the
+     * specified url with the specified request as a payload.
+     */
     public HttpURLConnection newInstance(String address, byte[] request)
         throws MalformedURLException, IOException, ProtocolException,
                AuthenticationException;
