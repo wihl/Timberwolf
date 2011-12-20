0ae7ed18f84d472f2571d773bfab36313abd16cc
Scott Dugas <scottd@softartisans.com>
2011-12-20 14:47:27 -0500

    added getFindItemsRequest test

--- src/test/java/com/softartisans/timberwolf/exchange/ExchangeMailStoreTest.java
+++ src/test/java/com/softartisans/timberwolf/exchange/ExchangeMailStoreTest.java
@@ -1,7 +1,15 @@
 package com.softartisans.timberwolf.exchange;
 
+import com.microsoft.schemas.exchange.services.x2006.messages.FindItemType;
+import com.microsoft.schemas.exchange.services.x2006.types.DefaultShapeNamesType;
+import com.microsoft.schemas.exchange.services.x2006.types.DistinguishedFolderIdNameType;
+import com.microsoft.schemas.exchange.services.x2006.types.DistinguishedFolderIdType;
+import com.microsoft.schemas.exchange.services.x2006.types.ItemQueryTraversalType;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Test for ExchangeMailStore, uses mock exchange service
@@ -19,4 +27,19 @@ public class ExchangeMailStoreTest
     {
 
     }
+
+    @Test
+    public void testFindItemRequest()
+    {
+        FindItemType findItem = FindItemType.Factory.newInstance();
+        findItem.setTraversal(ItemQueryTraversalType.SHALLOW);
+        findItem.addNewItemShape().setBaseShape(DefaultShapeNamesType.ID_ONLY);
+        DistinguishedFolderIdType folderId =
+                findItem.addNewParentFolderIds().addNewDistinguishedFolderId();
+        folderId.setId(DistinguishedFolderIdNameType.INBOX);
+        assertEquals(findItem.xmlText(),
+                     ExchangeMailStore.getFindItemsRequest(
+                             DistinguishedFolderIdNameType.INBOX).xmlText());
+    }
+
 }
