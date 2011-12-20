c1f6f646c4b450e669b2d3b7b4b901f11b40e656
Sean Kermes <seank@softartisans.com>
2011-12-20 14:04:40 -0500

    Stub of test fixture for ExchangeService.

---
+++ src/test/java/com/softartisans/timberwolf/exchange/ExchangeServiceTest.java
@@ -0,0 +1,20 @@
+package com.softartisans.timberwolf.exchange;
+
+import com.microsoft.schemas.exchange.services.x2006.messages.FindItemResponseType;
+import com.microsoft.schemas.exchange.services.x2006.messages.FindItemType;
+import com.microsoft.schemas.exchange.services.x2006.messages.GetItemResponseType;
+import com.microsoft.schemas.exchange.services.x2006.messages.GetItemType;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
+public class ExchangeServiceTest
+{
+    @Test
+    public void dummyTest()
+    {
+        assertTrue(true);
+    }
+}
