e707f20149926087a942dd53a5310615d173c59c
Nick Martin <nickm@softartisans.com>
2011-12-21 10:47:36 -0800

    Cleanup, removal of defunct tests, helpers.

--- src/test/java/com/softartisans/timberwolf/integrated/HBaseIntegrated.java
+++ src/test/java/com/softartisans/timberwolf/integrated/HBaseIntegrated.java
@@ -17,10 +17,20 @@ public class HBaseIntegrated
 
     private static HBaseManager hBaseManager;
     private static final String tableName = "testTable";
-    private static final String defaultRootDir =
-            "/usr/local/hbase-0.90.4/hbase";
-    private static final String defaultMaster = "127.0.0.1:60000";
-    private static List<String> columnFamilies;
+
+    private static void createTable(String tableName)
+    {
+        List<String> cfs = new ArrayList<String>();
+        cfs.add("cf");
+        hBaseManager.createTable(tableName,cfs);
+        Assert.assertTrue(hBaseManager.tableExists(tableName));
+    }
+
+    private static void deleteTable(String tableName)
+    {
+        hBaseManager.deleteTable(tableName);
+        Assert.assertFalse(hBaseManager.tableExists(tableName));
+    }
 
     /**
      * Fixture setup.
@@ -31,14 +41,6 @@ public class HBaseIntegrated
         hBaseManager = new HBaseManager(
                 IntegrationSettings.ZooKeeperQuorum,
                 IntegrationSettings.ZooKeeperClientPort);
-
-//        Configuration configuration = HBaseConfiguration.create();
-//        hBaseManager = new HBaseManager(configuration);
-//
-//        columnFamilies = new ArrayList<String>();
-//        columnFamilies.add("h");
-//
-//        hBaseManager.createTable(tableName, columnFamilies);
     }
 
     /**
@@ -47,53 +49,7 @@ public class HBaseIntegrated
     @AfterClass
     public static void tearDown()
     {
-//        hBaseManager.deleteTable(tableName);
         hBaseManager.close();
-    }
-
-    /**
-     * Tests getting a remote table instance.
-     */
-    @Test
-    public void testGetTableRemotely()
-    {
-        IHBaseTable table = hBaseManager.getTable(tableName);
-        Assert.assertEquals(tableName, table.getName());
-    }
-
-    /**
-     * Tests creating and deleting a table remotely.
-     */
-    @Test
-    public void testCreateAndDeleteTable()
-    {
-        String table = "aNewTable";
-        List<String> columnFamilies = new ArrayList<String>();
-        columnFamilies.add("cf");
-        hBaseManager.createTable(table, columnFamilies);
-        hBaseManager.deleteTable(table);
-    }
-
-    /**
-     * Simple test to show that we can make a HBaseManager for a specific HBase
-     * instance.
-     */
-    @Test
-    public void testCreateSpecificRemote()
-    {
-        HBaseManager hbase = new HBaseManager(defaultRootDir, defaultMaster);
-    }
-
-    /**
-     * Tests that we can perform actions on a specific HBase table.
-     */
-    @Test
-    public void testCreateTableSpecificRemote()
-    {
-        String aTable = "aTempTable";
-        HBaseManager hbase = new HBaseManager(defaultRootDir, defaultMaster);
-        hbase.createTable(aTable, columnFamilies);
-        hbase.deleteTable(aTable);
     }
 
     /**
@@ -102,13 +58,27 @@ public class HBaseIntegrated
     @Test
     public void testRemoteConnection()
     {
-        String tableName = "testTable";
+        String tableName = "HBaseIntegratedtestRemoteConnection";
 
-        List<String> cfs = new ArrayList<String>();
-        cfs.add("cf");
-        hBaseManager.createTable(tableName,cfs);
-        Assert.assertTrue(hBaseManager.tableExists(tableName));
-        hBaseManager.deleteTable(tableName);
-        Assert.assertFalse(hBaseManager.tableExists(tableName));
+        createTable(tableName);
+        deleteTable(tableName);
     }
+
+    /**
+     * Tests getting a remote table instance.
+     */
+    @Test
+    public void testRemoteGetTable()
+    {
+        String tableName = "HBaseIntegratedtestRemoteGetTable";
+
+        createTable(tableName);
+
+        IHBaseTable table = hBaseManager.getTable(tableName);
+        Assert.assertEquals(tableName, table.getName());
+        table.close();
+
+        deleteTable(tableName);
+    }
+
 }
