a4ea0f178b745f57d8ce7a9f0ad23dd136e804f3
Sean Kermes <seank@softartisans.com>
2011-12-21 11:53:32 -0500

    Testing getItem

--- src/test/java/com/softartisans/timberwolf/exchange/ExchangeServiceTest.java
+++ src/test/java/com/softartisans/timberwolf/exchange/ExchangeServiceTest.java
@@ -5,6 +5,7 @@ import com.microsoft.schemas.exchange.services.x2006.messages.FindItemType;
 import com.microsoft.schemas.exchange.services.x2006.messages.FindItemDocument;
 import com.microsoft.schemas.exchange.services.x2006.messages.GetItemResponseType;
 import com.microsoft.schemas.exchange.services.x2006.messages.GetItemType;
+import com.microsoft.schemas.exchange.services.x2006.messages.GetItemDocument;
 import com.microsoft.schemas.exchange.services.x2006.messages.FindItemResponseMessageType;
 import com.microsoft.schemas.exchange.services.x2006.types.FindItemParentType;
 import com.microsoft.schemas.exchange.services.x2006.types.MessageType;
@@ -61,6 +62,78 @@ public class ExchangeServiceTest
         "   </m:FindItemResponseMessage>\n" +
         "</m:ResponseMessages>\n" +
         "</m:FindItemResponse>";
+    private static final String getItemRequest =
+        "<GetItem" +
+        " xmlns=\"http://schemas.microsoft.com/exchange/services/2006/messages\"" +
+        " xmlns:t=\"http://schemas.microsoft.com/exchange/services/2006/types\">" +
+        "  <ItemShape>" +
+        "    <t:BaseShape>Default</t:BaseShape>" +
+        "    <t:IncludeMimeContent>true</t:IncludeMimeContent>" +
+        "  </ItemShape>" +
+        "  <ItemIds>" +
+        "    <t:ItemId Id=\"AAAlAF\" ChangeKey=\"CQAAAB\"/>" +
+        "  </ItemIds>" +
+        "</GetItem>";
+    private static final String getItemResponse =
+        "<GetItemResponse xmlns:m=\"http://schemas.microsoft.com/exchange/services/2006/messages\"" +
+        " xmlns:t=\"http://schemas.microsoft.com/exchange/services/2006/types\"" +
+        " xmlns=\"http://schemas.microsoft.com/exchange/services/2006/messages\">" +
+        "  <m:ResponseMessages>" +
+        "    <m:GetItemResponseMessage ResponseClass=\"Success\">" +
+        "      <m:ResponseCode>NoError</m:ResponseCode>" +
+        "      <m:Items>" +
+        "        <t:Message>" +
+        "          <t:MimeContent CharacterSet=\"UTF-8\">UmVjZWl</t:MimeContent>" +
+        "          <t:ItemId Id=\"AAAlAFVz\" ChangeKey=\"CQAAAB\" />" +
+        "          <t:Subject />" +
+        "          <t:Sensitivity>Normal</t:Sensitivity>" +
+        "          <t:Body BodyType=\"HTML\">" +
+        "           <![CDATA[" +
+        "            <html dir=\"ltr\">" +
+        "              <head>" +
+        "                <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">" +
+        "                  <meta content=\"MSHTML 6.00.3790.2759\" name=\"GENERATOR\">" +
+        "                    <style title=\"owaParaStyle\">P { MARGIN-TOP: 0px; MARGIN-BOTTOM: 0px } </style>" +
+        "                  </head>" +
+        "              <body ocsi=\"x\">" +
+        "                <div dir=\"ltr\">" +
+        "                  <font face=\"Tahoma\" color=\"#000000\" size=\"2\"></font>&nbsp;" +
+        "                </div>" +
+        "              </body>" +
+        "            </html>" +
+        "           ]]>" +
+        "          </t:Body>" +
+        "          <t:Size>881</t:Size>" +
+        "          <t:DateTimeSent>2006-10-28T01:37:06Z</t:DateTimeSent>" +
+        "          <t:DateTimeCreated>2006-10-28T01:37:06Z</t:DateTimeCreated>" +
+        "          <t:ResponseObjects>" +
+        "            <t:ReplyToItem />" +
+        "            <t:ReplyAllToItem />" +
+        "            <t:ForwardItem />" +
+        "          </t:ResponseObjects>" +
+        "          <t:HasAttachments>false</t:HasAttachments>" +
+        "          <t:ToRecipients>" +
+        "            <t:Mailbox>" +
+        "              <t:Name>User1</t:Name>" +
+        "              <t:EmailAddress>User1@example.com</t:EmailAddress>" +
+        "              <t:RoutingType>SMTP</t:RoutingType>" +
+        "            </t:Mailbox>" +
+        "          </t:ToRecipients>" +
+        "          <t:IsReadReceiptRequested>false</t:IsReadReceiptRequested>" +
+        "          <t:IsDeliveryReceiptRequested>false</t:IsDeliveryReceiptRequested>" +
+        "          <t:From>" +
+        "            <t:Mailbox>" +
+        "              <t:Name>User2</t:Name>" +
+        "              <t:EmailAddress>User2@example.com</t:EmailAddress>" +
+        "              <t:RoutingType>SMTP</t:RoutingType>" +
+        "            </t:Mailbox>" +
+        "          </t:From>" +
+        "          <t:IsRead>false</t:IsRead>" +
+        "        </t:Message>" +
+        "      </m:Items>" +
+        "    </m:GetItemResponseMessage>" +
+        "  </m:ResponseMessages>" +
+        "</GetItemResponse>";
     
     private static String soap(String body)
     {
@@ -85,5 +158,25 @@ public class ExchangeServiceTest
                                         .getEnvelope().getBody().getFindItemResponse();
 
         assertEquals(expected.xmlText(), response.xmlText());
+    }
+
+    @Test
+    public void testGetItem()
+        throws UnsupportedEncodingException, XmlException,
+               HttpUrlConnectionCreationException, IOException
+    {
+        MockHttpUrlConnectionFactory factory = new MockHttpUrlConnectionFactory();
+        factory.forRequest(url, soap(getItemRequest).getBytes("UTF-8"))
+               .respondWith(HttpURLConnection.HTTP_OK, soap(getItemResponse).getBytes("UTF-8"));
+        
+        GetItemType getReq = GetItemDocument.Factory.parse(getItemRequest).getGetItem();
+
+        ExchangeService service = new ExchangeService(url, factory);
+        GetItemResponseType response = service.getItem(getReq);
+
+        GetItemResponseType expected = EnvelopeDocument.Factory.parse(soap(getItemResponse))
+                                       .getEnvelope().getBody().getGetItemResponse();
+
+        assertEquals(expected.toString(), response.toString());
     }
 }
