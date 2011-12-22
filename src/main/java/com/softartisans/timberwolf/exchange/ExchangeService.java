8a823118606c1d1eab2263ff600aa2f263d8a423
Sean Kermes <seank@softartisans.com>
2011-12-22 11:17:13 -0500

    Simplifying the exception footprint of ExchangeService.
    I'm using the HttpUrlConnectionCreationException inappropriately
    in a number of places here.  That'll be fixed before I'm done.

--- src/main/java/com/softartisans/timberwolf/exchange/ExchangeService.java
+++ src/main/java/com/softartisans/timberwolf/exchange/ExchangeService.java
@@ -11,6 +11,10 @@ import org.xmlsoap.schemas.soap.envelope.EnvelopeType;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.util.Scanner;
+import java.io.InputStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * ExchangeService handles packing xmlbeans objects into a SOAP envelope,
@@ -19,6 +23,8 @@ import java.net.HttpURLConnection;
  */
 public class ExchangeService
 {
+    private static final Logger LOG = LoggerFactory.getLogger(ExchangeService.class);
+
     private static final String DECLARATION = "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
     private static final String SOAP_ENCODING = "UTF-8";
 
@@ -41,28 +47,85 @@ public class ExchangeService
 
     /** Sends a SOAP envelope request and returns the response. */
     private EnvelopeDocument sendRequest(EnvelopeDocument envelope)
-        throws UnsupportedEncodingException, XmlException, HttpUrlConnectionCreationException, IOException
+        throws HttpErrorException, HttpUrlConnectionCreationException
     {
         String request = DECLARATION + envelope.xmlText();
-        // TODO: log request.
+        LOG.trace("Sending SOAP request to {}.  SOAP envelope:", endpoint);
+        LOG.trace(request);
 
-        HttpURLConnection conn = connectionFactory.newInstance(endpoint, request.getBytes(SOAP_ENCODING));
-        if (conn.getResponseCode() == HttpURLConnection.HTTP_OK)
+        HttpURLConnection conn;
+        int code;
+        try
         {
-            EnvelopeDocument response = EnvelopeDocument.Factory.parse(conn.getInputStream());
+            conn = connectionFactory.newInstance(endpoint, request.getBytes(SOAP_ENCODING));
+            code = conn.getResponseCode();
+        }
+        catch (UnsupportedEncodingException e)
+        {
+            LOG.error("Request body could not be encoded into " + SOAP_ENCODING, e);
+            throw new HttpUrlConnectionCreationException("Error encoding request body.", e);
+        }
+        catch (IOException e)
+        {
+            LOG.error("There was an error getting the HTTP status code for the response.", e);
+            throw new HttpUrlConnectionCreationException("Error getting HTTP status code.", e);
+        }
+
+        InputStream responseData;
+        try
+        {
+           responseData = conn.getInputStream(); 
+        }
+        catch (IOException e)
+        {
+            LOG.error("There was an error getting the input stream for the response.", e);
+            throw new HttpUrlConnectionCreationException("Error getting input stream.", e);
+        }
+        
+        if (code == HttpURLConnection.HTTP_OK)
+        {
+            EnvelopeDocument response;
+            try
+            {
+                response = EnvelopeDocument.Factory.parse(responseData);
+            }
+            catch (IOException e)
+            {
+                LOG.error("There was an error reading from the response stream.", e);
+                throw new HttpUrlConnectionCreationException("Error reading response stream.", e);
+            }
+            catch (XmlException e)
+            {
+                LOG.error("There was an error parsing the SOAP response from Exchange.");
+                LOG.debug("Response body:");
+                // Why this works: http://stackoverflow.com/questions/309424/in-java-how-do-i-read-convert-an-inputstream-to-a-string#5445161
+                LOG.debug(new Scanner(responseData).useDelimiter("\\A").next());
+                throw new HttpUrlConnectionCreationException("Error parsing SOAP response.", e);
+            }
+            
+            LOG.trace("SOAP response received from {}.  SOAP envelope:", endpoint);
+            LOG.trace(response.xmlText());
             return response;
         }
         else
         {
-            // TODO: log error
-            // And return something better.
-            return EnvelopeDocument.Factory.newInstance();
+            LOG.error("Server responded with HTTP error code {}.", code);
+            if (!LOG.isTraceEnabled())
+            {
+                LOG.debug("Request that generated the error:");
+                LOG.debug(request);
+            }
+            
+            LOG.debug("Error response body:");
+            // Why this works: http://stackoverflow.com/questions/309424/in-java-how-do-i-read-convert-an-inputstream-to-a-string#5445161
+            LOG.debug(new Scanner(responseData).useDelimiter("\\A").next());            
+            throw new HttpErrorException(code);
         }
     }
 
     /** Returns the results of a find item request. */
     public FindItemResponseType findItem(FindItemType findItem)
-        throws UnsupportedEncodingException, XmlException, HttpUrlConnectionCreationException, IOException
+        throws HttpUrlConnectionCreationException, HttpErrorException
     {
         EnvelopeDocument request = EnvelopeDocument.Factory.newInstance();
         EnvelopeType envelope = request.addNewEnvelope();
@@ -74,8 +137,7 @@ public class ExchangeService
 
     /** Returns the results of a get item request. */
     public GetItemResponseType getItem(GetItemType getItem)
-        throws UnsupportedEncodingException, XmlException,
-               HttpUrlConnectionCreationException, IOException
+        throws HttpUrlConnectionCreationException, HttpErrorException
     {
         EnvelopeDocument request = EnvelopeDocument.Factory.newInstance();
         EnvelopeType envelope = request.addNewEnvelope();
---
+++ src/main/java/com/softartisans/timberwolf/exchange/HttpErrorException.java
@@ -0,0 +1,21 @@
+package com.softartisans.timberwolf.exchange;
+
+/** 
+ * Exception that gets thrown when an HTTP request returns 
+ * an error (non-200) response.
+ */
+public class HttpErrorException extends Exception
+{
+    private int errorCode;
+
+    public HttpErrorException(int error)
+    {
+        super("There was an HTTP " + error + " error while sending a request.");
+        errorCode = error;
+    }
+
+    public int getErrorCode()
+    {
+        return errorCode;
+    }
+}
