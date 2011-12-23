2fd22bc7332ebb3289e467aeb64eeccb6023f5cf
Scott Dugas <scottd@softartisans.com>
2011-12-23 13:35:57 -0500

    Switched to using rules

--- src/test/java/com/softartisans/timberwolf/integrated/TestHBaseIntegration.java
+++ src/test/java/com/softartisans/timberwolf/integrated/TestHBaseIntegration.java
@@ -4,11 +4,9 @@ import com.softartisans.timberwolf.hbase.HBaseManager;
 import com.softartisans.timberwolf.hbase.IHBaseTable;
 import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TestName;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,7 +14,8 @@ import java.util.List;
 public class TestHBaseIntegration
 {
     @Rule
-    public TestName name = new TestName();
+    public IntegrationTestProperties properties = new IntegrationTestProperties(ZOO_KEEPER_QUORUM_PROPERTY_NAME,
+                                                                        ZOO_KEEPER_CLIENT_PORT_PROPERTY_NAME);
     private static HBaseManager hBaseManager;
     private static final String tableName = "testTable";
     private static final String ZOO_KEEPER_QUORUM_PROPERTY_NAME = "ZooKeeperQuorum";
@@ -42,18 +41,12 @@ public class TestHBaseIntegration
     @BeforeClass
     public static void classSetUp()
     {
-        String zooKeeperQuorum = PropertiesForTests.getProperty(ZOO_KEEPER_QUORUM_PROPERTY_NAME);
-        String zooKeeperClientPort = PropertiesForTests.getProperty(ZOO_KEEPER_CLIENT_PORT_PROPERTY_NAME);
+        String zooKeeperQuorum = IntegrationTestProperties.getProperty(ZOO_KEEPER_QUORUM_PROPERTY_NAME);
+        String zooKeeperClientPort = IntegrationTestProperties.getProperty(ZOO_KEEPER_CLIENT_PORT_PROPERTY_NAME);
         if (zooKeeperQuorum != null && zooKeeperClientPort != null)
         {
             hBaseManager = new HBaseManager(zooKeeperQuorum, zooKeeperClientPort);
         }
-    }
-
-    @Before
-    public void setUp()
-    {
-        PropertiesForTests.assume(name, ZOO_KEEPER_QUORUM_PROPERTY_NAME, ZOO_KEEPER_CLIENT_PORT_PROPERTY_NAME);
     }
 
     /**
