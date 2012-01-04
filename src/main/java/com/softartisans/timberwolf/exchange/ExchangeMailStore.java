01d7184ec32acce82033bf4557c46b8250bb0ba9
Nick Martin <nickm@softartisans.com>
2012-01-04 11:04:09 -0500

    FindFolders.

--- src/main/java/com/softartisans/timberwolf/exchange/ExchangeMailStore.java
+++ src/main/java/com/softartisans/timberwolf/exchange/ExchangeMailStore.java
@@ -2,14 +2,7 @@ package com.softartisans.timberwolf.exchange;
 
 import com.cloudera.alfredo.client.AuthenticationException;
 
-import com.microsoft.schemas.exchange.services.x2006.messages.ArrayOfResponseMessagesType;
-import com.microsoft.schemas.exchange.services.x2006.messages.FindItemResponseMessageType;
-import com.microsoft.schemas.exchange.services.x2006.messages.FindItemResponseType;
-import com.microsoft.schemas.exchange.services.x2006.messages.FindItemType;
-import com.microsoft.schemas.exchange.services.x2006.messages.GetItemResponseType;
-import com.microsoft.schemas.exchange.services.x2006.messages.GetItemType;
-import com.microsoft.schemas.exchange.services.x2006.messages.ItemInfoResponseMessageType;
-import com.microsoft.schemas.exchange.services.x2006.messages.ResponseCodeType;
+import com.microsoft.schemas.exchange.services.x2006.messages.*;
 import com.microsoft.schemas.exchange.services.x2006.types.*;
 
 import com.softartisans.timberwolf.MailStore;
@@ -202,8 +195,7 @@ public class ExchangeMailStore implements MailStore
     private static Vector<String> findItems(final ExchangeService exchangeService, FindItemType findItem)
             throws ServiceCallException, HttpErrorException
     {
-        FindItemResponseType response =
-                exchangeService.findItem(findItem);
+        FindItemResponseType response = exchangeService.findItem(findItem);
 
         if (response == null)
         {
@@ -228,6 +220,96 @@ public class ExchangeMailStore implements MailStore
             }
         }
         return items;
+    }
+
+    /**
+     * Creates a FindFolderType for the given distinguished folder.
+     *
+     * @param folder The distinguished folder.
+     * @return The FindItemType for the distinguished folder.
+     */
+    static FindFolderType getFindFoldersRequest(final DistinguishedFolderIdNameType.Enum folder)
+    {
+        FindFolderType findFolder = getFindFolderType();
+
+        DistinguishedFolderIdType folderId = findFolder.addNewParentFolderIds().addNewDistinguishedFolderId();
+        folderId.setId(folder);
+
+        return findFolder;
+    }
+
+    /**
+     * Creates a FindFolderType for the given folder.
+     *
+     * @param folderId The folder id for the folder from which to get ids.
+     * @return The FindFolderType for the given folder..
+     */
+    static FindFolderType getFindFoldersRequest(final String folderId)
+    {
+        FindFolderType findFolder = getFindFolderType();
+
+        FolderIdType folder = findFolder.addNewParentFolderIds().addNewFolderId();
+        folder.setId(folderId);
+
+        return findFolder;
+    }
+
+    /**
+     * Creates a FindFolderType with a default behavior. The FolderQueryTraversal is set to deep.
+     * @return A FindFolderType.
+     */
+    private static final FindFolderType getFindFolderType()
+    {
+        FindFolderType findFolder = FindFolderType.Factory.newInstance();
+        findFolder.setTraversal(FolderQueryTraversalType.DEEP);
+        findFolder.addNewFolderShape().setBaseShape(DefaultShapeNamesType.ALL_PROPERTIES);
+
+        return findFolder;
+    }
+
+    static Vector<String> findFolders(final ExchangeService exchangeService, String folderId)
+            throws ServiceCallException, HttpErrorException
+    {
+        return findFolders(exchangeService, getFindFoldersRequest(folderId));
+    }
+
+    /**
+     * Gets a list of folder ids for the folder specified by findFolder.
+     *
+     * @param exchangeService The Exchange service to use.
+     * @param findFolder The FindFolder request to use.
+     * @return A list of all immediate child folders.
+     * @throws ServiceCallException If the Exchange service could not be connected to.
+     * @throws HttpErrorException If the response could not be parsed.
+     */
+    private static Vector<String> findFolders(final ExchangeService exchangeService, FindFolderType findFolder)
+            throws ServiceCallException, HttpErrorException
+    {
+        FindFolderResponseType response = exchangeService.findFolder(findFolder);
+
+        if (response == null)
+        {
+            LOG.debug("Exchange service returned null find folder response.");
+            throw new ServiceCallException(ServiceCallException.Reason.OTHER, "Null response from Exchange service.");
+        }
+
+        ArrayOfResponseMessagesType array = response.getResponseMessages();
+        Vector<String> folders = new Vector<String>();
+        for (FindFolderResponseMessageType message :array.getFindFolderResponseMessageArray())
+        {
+            ResponseCodeType.Enum errorCode = message.getResponseCode();
+            if (errorCode != null && errorCode != ResponseCodeType.NO_ERROR)
+            {
+                LOG.debug(errorCode.toString());
+                throw new ServiceCallException(errorCode, "SOAP response contained an error.");
+            }
+
+            for (FolderType folder : message.getRootFolder().getFolders().getFolderArray())
+            {
+                folders.add(folder.getFolderId().getId());
+            }
+        }
+        return folders;
     }
 
     /**
