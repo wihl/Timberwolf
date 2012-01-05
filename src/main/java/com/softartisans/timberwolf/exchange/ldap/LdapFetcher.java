2c0f67bd549ece8a6ab9857d0d1af8276c461a3c
Cameron Fischer <cameronf@softartisans.com>
2012-01-05 14:14:59 -0500

    Automatic LDAP discovery implemented

--- src/main/java/com/softartisans/timberwolf/exchange/ldap/LdapFetcher.java
+++ src/main/java/com/softartisans/timberwolf/exchange/ldap/LdapFetcher.java
@@ -6,40 +6,57 @@ import java.util.Hashtable;
 
 public class LdapFetcher {
 
+	public static String getSecurityMechanisms(String domainName) {
+        try {
+            Hashtable<String, String> defEnv = defaultEnvironment(domainName);
+            DirContext ctx = new InitialDirContext(defEnv);
+
+            Attributes attrs = ctx.getAttributes(defEnv.get(Context.PROVIDER_URL),
+                                                 new String[]{"supportedSASLMechanisms"});
+            return stringifyAttributes(attrs, "supportedSASLMechanisms");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "failed";
+        }
+    }
+
+    private static Hashtable<String, String> defaultEnvironment(String domainName)
+    {
+        Hashtable<String, String> env = new Hashtable<String, String>();
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+        // The secrets about this PROVIDER_URL are contained within:
+        // http://docs.oracle.com/javase/6/docs/technotes/guides/jndi/jndi-ldap.html
+        // Check out the part about "automatic discovery of LDAP services".
+        // Because that's what's happening here.
+        env.put(Context.PROVIDER_URL, getProviderDiscoveryURL(domainName));
+        env.put(Context.SECURITY_AUTHENTICATION, "simple");
+        env.put(Context.SECURITY_PRINCIPAL, "developer@INT.TARTARUS.COM");
+        env.put(Context.SECURITY_CREDENTIALS, "pass@word1");
+        return env;
+    }
+
     public static String getAllUsers(String domainName) {
         StringBuilder output = new StringBuilder();
-        
-        String userDNDirectory = getUserDNDirectory(domainName);
-
+        Hashtable<String, String> defEnv = defaultEnvironment(domainName);
         try {
-            String url = "ldap://devexch01.int.tartarus.com";
-            Hashtable<String, String> env = new Hashtable<String, String>();
-            env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-            env.put(Context.PROVIDER_URL, url);
-            env.put(Context.SECURITY_AUTHENTICATION, "simple");
-            env.put(Context.SECURITY_PRINCIPAL, "developer@INT.TARTARUS.COM");
-            env.put(Context.SECURITY_CREDENTIALS, "pass@word1");
-            DirContext context = new InitialDirContext(env);
+            DirContext context = new InitialDirContext(defEnv);
 
             String wantedAttribute = "userPrincipalName";
             SearchControls ctrl = new SearchControls();
             String[] attributeFilter = { wantedAttribute };
             ctrl.setReturningAttributes(attributeFilter);
             ctrl.setSearchScope(SearchControls.SUBTREE_SCOPE);
-            NamingEnumeration<?> enumeration = context.search(userDNDirectory, "(objectClass=person)", ctrl);
+            NamingEnumeration<?> enumeration = context.search("CN=Users", "(objectClass=person)", ctrl);
             while (enumeration.hasMore()) {
                 SearchResult result = (SearchResult) enumeration.next();
                 Attributes attribs = result.getAttributes();
-                Attribute attrib = attribs.get(wantedAttribute);
-                if (attrib != null) {
-                  NamingEnumeration<?> values = ((BasicAttribute)attrib).getAll();
-                  while (values.hasMore()) {
-                    if (output.length() > 0) {
-                      output.append("\n");
-                    }
-                    output.append(values.next().toString());
-                  }
+                String values = stringifyAttributes(attribs, wantedAttribute);
+                if (output.length() > 0)
+                {
+                    output.append("\n");
                 }
+                output.append(values);
             }
 
         } catch (Exception e) {
@@ -47,18 +64,59 @@ public class LdapFetcher {
         }
         return output.toString();
     }
-    
+
+    private static String stringifyAttributes(Attributes attribs, String wantedAttribute) throws NamingException
+    {
+        Attribute attrib = attribs.get(wantedAttribute);
+        StringBuilder output = new StringBuilder();
+        if (attrib != null) {
+            NamingEnumeration<?> values = ((BasicAttribute)attrib).getAll();
+            while (values.hasMore()) {
+                if (output.length() > 0) {
+                    output.append("\n");
+                }
+                output.append(values.next().toString());
+            }
+        }
+        return output.toString();
+    }
+
+    private static String getProviderDiscoveryURL(String domainName)
+    {
+        StringBuilder userDNBuilder = new StringBuilder();
+        userDNBuilder.append("ldap:///");
+        String[] bits = domainName.split("\\.");
+        boolean firstBit = true;
+        for (String bit : bits)
+        {
+            if (firstBit)
+            {
+                firstBit = false;
+            }
+            else
+            {
+                userDNBuilder.append(',');
+            }
+            userDNBuilder.append("dc=");
+            userDNBuilder.append(bit);
+        }
+        return userDNBuilder.toString();
+    }
+
     private static String getUserDNDirectory(String domainName)
     {
-    	String[] bits = domainName.split("\\.");
-    	StringBuilder userDNBuilder = new StringBuilder();
-    	userDNBuilder.append("CN=Users");
-    	for (String bit : bits)
-    	{
-    		userDNBuilder.append(",DC=");
-    		userDNBuilder.append(bit);
-    	}
-    	return userDNBuilder.toString();
+        StringBuilder userDNBuilder = new StringBuilder();
+        userDNBuilder.append("CN=Users");
+        if (domainName != null && domainName.length() != 0)
+        {
+            String[] bits = domainName.split("\\.");
+            for (String bit : bits)
+            {
+                userDNBuilder.append(",DC=");
+                userDNBuilder.append(bit);
+            }
+        }
+        return userDNBuilder.toString();
     }
 
     public LdapFetcher() {}
--- src/test/java/com/softartisans/timberwolf/exchange/ldap/LdapFetcherTest.java
+++ src/test/java/com/softartisans/timberwolf/exchange/ldap/LdapFetcherTest.java
@@ -3,11 +3,18 @@ package com.softartisans.timberwolf.exchange.ldap;
 import org.junit.Test;
 
 public class LdapFetcherTest {
-	
+	final static String domainTartarus = "int.tartarus.com";
 	@Test
 	public void firstTest()
 	{
-		String rtn = LdapFetcher.getAllUsers("int.tartarus.com");
+		String rtn = LdapFetcher.getAllUsers(domainTartarus);
+		System.out.print(rtn);
+	}
+
+	@Test
+	public void securityTest()
+	{
+		String rtn = LdapFetcher.getSecurityMechanisms(domainTartarus);
 		System.out.print(rtn);
 	}
 }
