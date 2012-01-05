1ce2a4935cee944dd05c1933b38a6bb60133cf2a
Sean Kermes <seank@softartisans.com>
2012-01-05 14:37:05 -0500

    Testing that the chain handles an empty user.

--- src/test/java/com/softartisans/timberwolf/exchange/EmailIteratorChainTest.java
+++ src/test/java/com/softartisans/timberwolf/exchange/EmailIteratorChainTest.java
@@ -1,21 +1,37 @@
 package com.softartisans.timberwolf.exchange;
 
+import com.microsoft.schemas.exchange.services.x2006.messages.ArrayOfResponseMessagesType;
+import com.microsoft.schemas.exchange.services.x2006.messages.FindItemResponseMessageType;
 import com.microsoft.schemas.exchange.services.x2006.messages.FindItemResponseType;
 import com.microsoft.schemas.exchange.services.x2006.messages.FindItemType;
-import com.microsoft.schemas.exchange.services.x2006.messages.FindItemDocument;
 import com.microsoft.schemas.exchange.services.x2006.messages.GetItemResponseType;
 import com.microsoft.schemas.exchange.services.x2006.messages.GetItemType;
-import com.microsoft.schemas.exchange.services.x2006.messages.GetItemDocument;
-import com.microsoft.schemas.exchange.services.x2006.messages.FindItemResponseMessageType;
-import com.microsoft.schemas.exchange.services.x2006.types.FindItemParentType;
-import com.microsoft.schemas.exchange.services.x2006.types.MessageType;
+import com.microsoft.schemas.exchange.services.x2006.messages.ItemInfoResponseMessageType;
 import com.microsoft.schemas.exchange.services.x2006.messages.ResponseCodeType;
+import com.microsoft.schemas.exchange.services.x2006.types.ArrayOfRealItemsType;
+import com.microsoft.schemas.exchange.services.x2006.types.DefaultShapeNamesType;
+import com.microsoft.schemas.exchange.services.x2006.types.DistinguishedFolderIdNameType;
+import com.microsoft.schemas.exchange.services.x2006.types.DistinguishedFolderIdType;
+import com.microsoft.schemas.exchange.services.x2006.types.FindItemParentType;
+import com.microsoft.schemas.exchange.services.x2006.types.ItemIdType;
+import com.microsoft.schemas.exchange.services.x2006.types.ItemQueryTraversalType;
+import com.microsoft.schemas.exchange.services.x2006.types.MessageType;
+import com.microsoft.schemas.exchange.services.x2006.types.NonEmptyArrayOfBaseItemIdsType;
 import org.xmlsoap.schemas.soap.envelope.EnvelopeDocument;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-import static org.mockito.Mockito.*;
+import static com.softartisans.timberwolf.exchange.IsXmlBeansRequest.LikeThis;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.net.HttpURLConnection;
 import java.io.UnsupportedEncodingException;
@@ -34,6 +50,24 @@ public class EmailIteratorChainTest
     {
         ExchangeService service = mock(ExchangeService.class);
         ArrayList<String> users = new ArrayList<String>();
+        EmailIteratorChain chain = new EmailIteratorChain(service, users);
+
+        assertFalse(chain.hasNext());
+        assertNull(chain.next());
+    }
+
+    @Test
+    public void testChainOneEmptyUser() throws ServiceCallException, HttpErrorException
+    {
+        ExchangeService service = mock(ExchangeService.class);
+        FindItemResponseType response = mock(FindItemResponseType.class);
+        ArrayOfResponseMessagesType msgArr = mock(ArrayOfResponseMessagesType.class);
+        when(msgArr.getFindItemResponseMessageArray()).thenReturn(new FindItemResponseMessageType[] { });
+        when(response.getResponseMessages()).thenReturn(msgArr);
+        when(service.findItem(any(FindItemType.class), eq("bkerr@INT.TARTARUS.COM"))).thenReturn(response);
+
+        ArrayList<String> users = new ArrayList<String>();
+        users.add("bkerr@INT.TARTARUS.COM");
         EmailIteratorChain chain = new EmailIteratorChain(service, users);
 
         assertFalse(chain.hasNext());
