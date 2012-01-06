f36d24056cfc7560f2e30d0523bf49030de51270
Nick Martin <nickm@softartisans.com>
2012-01-06 16:45:40 -0500

    UserTimeUpdaters. Setting up to add get to HBaseTable.

---
+++ src/main/java/com/softartisans/timberwolf/UserTimeUpdater.java
@@ -0,0 +1,22 @@
+package com.softartisans.timberwolf;
+
+import org.joda.time.DateTime;
+
+/**
+ * Allows determining when a user was last updated and setting that the user has been updated.
+ */
+public interface UserTimeUpdater
+{
+    /**
+     * Determines when the given user was last updated.
+     * @return When the user was last updated.
+     */
+    DateTime LastUpdated(String user);
+
+    /**
+     * Sets that the user has been updated at this datetime.
+     * @param user The user who has been updated.
+     * @param dateTime The datetime of the update.
+     */
+    void Updated(String user, DateTime dateTime);
+}
---
+++ src/main/java/com/softartisans/timberwolf/hbase/HBaseUserTimeUpdater.java
@@ -0,0 +1,49 @@
+package com.softartisans.timberwolf.hbase;
+
+import com.softartisans.timberwolf.UserTimeUpdater;
+import org.joda.time.DateTime;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * An implementation of a UserTimeUpdater which stores the appropriate timings in HBase.
+ */
+public class HBaseUserTimeUpdater implements UserTimeUpdater
+{
+    /** The HBaseManager for the HBase instance wherein our timings will be stored. */
+    private HBaseManager manager;
+
+    /** The table wherein our timings are stored. */
+    private IHBaseTable table;
+
+    private final String TIME_COLUMN_FAMILY = "t";
+
+    /**
+     * Constructs a HBaseUserTimeUpdater from a HBaseManager and a given table name.
+     * @param hBaseManager The HBaseManager to use to store our timings.
+     * @param updateTable
+     */
+    public HBaseUserTimeUpdater(final HBaseManager hBaseManager, final String updateTable)
+    {
+        manager = hBaseManager;
+
+        List<String> columnFamilies = new ArrayList<String>();
+        columnFamilies.add(TIME_COLUMN_FAMILY)
+
+        /** If the table already exists it will be simply grabbed not recreated. */
+        table = hBaseManager.createTable(updateTable, columnFamilies);  
+    }
+
+    @Override
+    public DateTime LastUpdated(String user)
+    {
+        return null;
+    }
+
+    @Override
+    public void Updated(String user, DateTime dateTime)
+    {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
