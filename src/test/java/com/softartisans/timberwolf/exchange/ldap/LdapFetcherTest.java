04f9d2452d27ea7851e9a78967c6e43c0d525819
Cameron Fischer <cameronf@softartisans.com>
2012-01-09 11:40:22 -0500

    Corrected tabs to spaces in ldap tests

--- src/test/java/com/softartisans/timberwolf/exchange/ldap/LdapFetcherTest.java
+++ src/test/java/com/softartisans/timberwolf/exchange/ldap/LdapFetcherTest.java
@@ -5,12 +5,12 @@ import com.softartisans.timberwolf.exchange.PrincipalFetchException;
 import org.junit.Test;
 
 public class LdapFetcherTest {
-	final static String domainTartarus = "int.tartarus.com";
+    final static String domainTartarus = "int.tartarus.com";
 
-	@Test
-	public void firstTest()
-	{
-		try
+    @Test
+    public void firstTest()
+    {
+        try
         {
             printIterable(new LdapFetcher(domainTartarus).getPrincipals());
         }
@@ -18,26 +18,26 @@ public class LdapFetcherTest {
         {
             e.printStackTrace();
         }
-	}
+    }
 
-	@Test
-	public void securityTest()
-	{
-	    try
-	    {
-	        printIterable(LdapFetcher.getSecurityMechanisms(domainTartarus));
-	    }
-	    catch (PrincipalFetchException e)
-	    {
-	        e.printStackTrace();
-	    }
-	}
+    @Test
+    public void securityTest()
+    {
+        try
+        {
+            printIterable(LdapFetcher.getSecurityMechanisms(domainTartarus));
+        }
+        catch (PrincipalFetchException e)
+        {
+            e.printStackTrace();
+        }
+    }
 
-	private void printIterable(Iterable<String> iter)
-	{
-	    for (String str : iter)
-	    {
-	        System.out.println(str);
-	    }
-	}
+    private void printIterable(Iterable<String> iter)
+    {
+        for (String str : iter)
+        {
+            System.out.println(str);
+        }
+    }
 }
