18445bac9b6525ce7b75f8870e87823228d987f9
Cameron Fischer <cameronf@softartisans.com>
2012-01-10 11:55:35 -0500

    Everything works now

--- src/main/java/com/softartisans/timberwolf/exchange/ldap/LdapFetcher.java
+++ src/main/java/com/softartisans/timberwolf/exchange/ldap/LdapFetcher.java
@@ -2,59 +2,55 @@ package com.softartisans.timberwolf.exchange.ldap;
 
 import javax.naming.*;
 import javax.naming.directory.*;
+import javax.security.auth.Subject;
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
 
 import com.softartisans.timberwolf.exchange.PrincipalFetchException;
 import com.softartisans.timberwolf.exchange.PrincipalFetcher;
+import com.sun.security.auth.callback.TextCallbackHandler;
 
+import java.security.PrivilegedAction;
 import java.util.Collection;
 import java.util.Hashtable;
 import java.util.LinkedList;
 import java.util.List;
 
-public class LdapFetcher implements PrincipalFetcher {
+public class LdapFetcher implements PrincipalFetcher, PrivilegedAction<Iterable<String>> {
     final String _domainName;
+    final String _configurationEntry;
+    PrincipalFetchException fail;
 
-    public LdapFetcher(String domainName)
+    public LdapFetcher(String domainName, String configurationEntry)
     {
-        _domainName = domainName;
-    }
-
-    public static Iterable<String> getSecurityMechanisms(String domainName) throws PrincipalFetchException {
-        try {
-            Hashtable<String, String> defEnv = defaultEnvironment(domainName);
-            DirContext ctx = new InitialDirContext(defEnv);
-
-            Attributes attrs = ctx.getAttributes("ldap://devexch01.int.tartarus.com",
-                                                 new String[]{"supportedSASLMechanisms"});
-            List<String> rtnList = new LinkedList<String>();
-            stringifyAttributes(attrs, "supportedSASLMechanisms", rtnList);
-            return rtnList;
-
-        } catch (NamingException e) {
-            throw new PrincipalFetchException(e);
+        if (domainName == null || domainName.length() == 0)
+        {
+            throw new IllegalArgumentException("domainName cannot be empty");
         }
-    }
-
-    private static Hashtable<String, String> defaultEnvironment(String domainName)
-    {
-        Hashtable<String, String> env = new Hashtable<String, String>();
-        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-        // The secrets about this PROVIDER_URL are contained within:
-        // http://docs.oracle.com/javase/6/docs/technotes/guides/jndi/jndi-ldap.html
-        // Check out the part about "automatic discovery of LDAP services".
-        // Because that's what's happening here.
-        env.put(Context.PROVIDER_URL, getProviderDiscoveryURL(domainName));
-        //env.put(Context.PROVIDER_URL, "ldap://devexch01.int.tartarus.com");
-        env.put(Context.SECURITY_AUTHENTICATION, "GSSAPI");
-        //env.put(Context.SECURITY_AUTHENTICATION, "simple");
-        //env.put(Context.SECURITY_PRINCIPAL, "developer");
-        //env.put(Context.SECURITY_CREDENTIALS, "pass@word1");
-        return env;
+        if (configurationEntry == null || configurationEntry.length() == 0)
+        {
+            throw new IllegalArgumentException("configurationEntry cannot be empty");
+        }
+        _domainName = domainName;
+        _configurationEntry = configurationEntry;
     }
 
     public Iterable<String> getPrincipals() throws PrincipalFetchException
     {
-        Hashtable<String, String> defEnv = defaultEnvironment(_domainName);
+        LoginContext lc = loginAsDev(_configurationEntry);
+        Subject sbj = lc.getSubject();
+
+        Iterable<String> rtn = Subject.doAs(sbj, this);
+        if (fail != null)
+        {
+            throw fail;
+        }
+        return rtn;
+    }
+
+    @Override
+    public Iterable<String> run() {
+        Hashtable<String, String> defEnv = defaultEnvironment();
         List<String> rtnList = new LinkedList<String>();
         try {
             DirContext context = new InitialDirContext(defEnv);
@@ -72,9 +68,40 @@ public class LdapFetcher implements PrincipalFetcher {
             }
 
         } catch (NamingException e) {
-            throw new PrincipalFetchException(e);
+            e.printStackTrace();
+            fail = new PrincipalFetchException(e);
         }
         return rtnList;
+    }
+
+    private Hashtable<String, String> defaultEnvironment()
+    {
+        Hashtable<String, String> env = new Hashtable<String, String>();
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+        // The secrets about this PROVIDER_URL are contained within:
+        // http://docs.oracle.com/javase/6/docs/technotes/guides/jndi/jndi-ldap.html
+        // Check out the part about "automatic discovery of LDAP services".
+        // Because that's what's happening here.
+        env.put(Context.PROVIDER_URL, getProviderDiscoveryURL());
+        env.put(Context.SECURITY_AUTHENTICATION, "GSSAPI");
+        return env;
+    }
+
+    private static LoginContext loginAsDev(String configurationEntry) throws PrincipalFetchException
+    {
+        LoginContext lc = null;
+        try {
+            lc = new LoginContext(configurationEntry, new TextCallbackHandler());
+            // Attempt authentication
+            // We might want to do this in a "for" loop to give
+            // user more than one chance to enter correct username/password
+            lc.login();
+        } catch (LoginException le) {
+            System.err.println("Authentication attempt failed: " + le);
+            throw new PrincipalFetchException(le);
+        }
+
+        return lc;
     }
 
     private static void stringifyAttributes(Attributes attribs, String wantedAttribute, Collection<String> collector) throws NamingException
@@ -88,11 +115,15 @@ public class LdapFetcher implements PrincipalFetcher {
         }
     }
 
-    private static String getProviderDiscoveryURL(String domainName)
+    /**
+     * Given a domain name, this returns the url
+     * used to automatically discover the ldap server.
+     */
+    public String getProviderDiscoveryURL()
     {
         StringBuilder userDNBuilder = new StringBuilder();
         userDNBuilder.append("ldap:///");
-        String[] bits = domainName.split("\\.");
+        String[] bits = _domainName.split("\\.");
         boolean firstBit = true;
         for (String bit : bits)
         {
@@ -108,5 +139,28 @@ public class LdapFetcher implements PrincipalFetcher {
             userDNBuilder.append(bit);
         }
         return userDNBuilder.toString();
+    }
+
+    /** Queries the domain's LDAP server for supported authentication methods.
+     *
+     * I'm leaving this hear for the time being because it's quite likely
+     * we may face LDAP servers that don't support GSSAPI.
+     * @return An iterable listing all supported authentication methods.
+     * @throws PrincipalFetchException
+     */
+    public Iterable<String> getSecurityMechanisms() throws PrincipalFetchException {
+        try {
+            Hashtable<String, String> defEnv = defaultEnvironment();
+            DirContext ctx = new InitialDirContext(defEnv);
+
+            Attributes attrs = ctx.getAttributes(getProviderDiscoveryURL(),
+                                                 new String[]{"supportedSASLMechanisms"});
+            List<String> rtnList = new LinkedList<String>();
+            stringifyAttributes(attrs, "supportedSASLMechanisms", rtnList);
+            return rtnList;
+
+        } catch (NamingException e) {
+            throw new PrincipalFetchException(e);
+        }
     }
 }
--- src/test/java/com/softartisans/timberwolf/exchange/ldap/LdapFetcherTest.java
+++ src/test/java/com/softartisans/timberwolf/exchange/ldap/LdapFetcherTest.java
@@ -1,43 +1,80 @@
 package com.softartisans.timberwolf.exchange.ldap;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.*;
+
 import com.softartisans.timberwolf.exchange.PrincipalFetchException;
 
 import org.junit.Test;
 
 public class LdapFetcherTest {
     final static String domainTartarus = "int.tartarus.com";
+    final static String testConfigEntry = "Timberwolf";
 
     @Test
-    public void firstTest()
+    public void getProviderDiscoveryURLTest()
     {
-        try
-        {
-            printIterable(new LdapFetcher(domainTartarus).getPrincipals());
-        }
-        catch (PrincipalFetchException e)
-        {
-            e.printStackTrace();
-        }
+        LdapFetcher result = new LdapFetcher(domainTartarus, testConfigEntry);
+        Assert.assertEquals("ldap:///dc=int,dc=tartarus,dc=com",
+                            result.getProviderDiscoveryURL());
+    }
+    
+    @Test
+    public void getProviderDiscoveryURLEmptyTest()
+    {
+        LdapFetcher result = new LdapFetcher("tartarus", testConfigEntry);
+        Assert.assertEquals("ldap:///dc=tartarus",
+                            result.getProviderDiscoveryURL());
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void illegalDomainNameEmptyTest()
+    {
+        new LdapFetcher("", "someConfiguration");
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void illegalDomainNameNullTest()
+    {
+        new LdapFetcher(null, "someConfiguration");
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void illegalConfigurationEntryEmptyTest()
+    {
+        new LdapFetcher("tartarus", "");
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void illegalConfigurationEntryNullTest()
+    {
+        new LdapFetcher("tartarus", null);
+    }
+    
+    // I'm not sure yet how to test the bulk of the code.
+    /*
+    public void firstTest() throws PrincipalFetchException
+    {
+        List<String> results = asList(new LdapFetcher(domainTartarus, testConfigEntry).getPrincipals());
+        Assert.assertTrue(results.contains("BKerr@int.tartarus.com"));
     }
 
     @Test
-    public void securityTest()
+    public void securityTest() throws PrincipalFetchException
     {
-        try
-        {
-            printIterable(LdapFetcher.getSecurityMechanisms(domainTartarus));
-        }
-        catch (PrincipalFetchException e)
-        {
-            e.printStackTrace();
-        }
+        printIterable(new LdapFetcher(domainTartarus, testConfigEntry).getSecurityMechanisms());
     }
+    */
 
-    private void printIterable(Iterable<String> iter)
+    private List<String> asList(Iterable<String> iter)
     {
+        List<String> results = new ArrayList<String>();
         for (String str : iter)
         {
-            System.out.println(str);
+            results.add(str);
         }
+        return results;
     }
 }
