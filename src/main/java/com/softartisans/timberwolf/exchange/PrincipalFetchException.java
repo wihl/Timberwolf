cf93f2286cb3c4edc9852e251960dd2a6b746ae3
Cameron Fischer <cameronf@softartisans.com>
2012-01-10 14:02:33 -0500

    A few more LDAP corrections for checkstyle

--- src/main/java/com/softartisans/timberwolf/exchange/PrincipalFetchException.java
+++ src/main/java/com/softartisans/timberwolf/exchange/PrincipalFetchException.java
@@ -1,8 +1,12 @@
 package com.softartisans.timberwolf.exchange;
 
-public class PrincipalFetchException extends Exception {
-
-    public PrincipalFetchException(Exception innerException)
+/**
+ * This or a derivitive of this is thrown should an error occur fetching
+ * a complete list of principals from whatever derives PrincipalFetcher.
+ */
+public class PrincipalFetchException extends Exception
+{
+    public PrincipalFetchException(final Exception innerException)
     {
         super(innerException);
     }
--- src/main/java/com/softartisans/timberwolf/exchange/PrincipalFetcher.java
+++ src/main/java/com/softartisans/timberwolf/exchange/PrincipalFetcher.java
@@ -1,5 +1,6 @@
 package com.softartisans.timberwolf.exchange;
 
+/** This class will return a list of principals we should fetch emails for. */
 public interface PrincipalFetcher
 {
     Iterable<String> getPrincipals() throws PrincipalFetchException;
