4d354fbefa7f6b903c16107d3a30afe7cdc3919e
Scott Dugas <scottd@softartisans.com>
2012-01-16 15:28:59 -0500

    User iterator only warns if a user fails

--- src/main/java/com/softartisans/timberwolf/exchange/UserIterator.java
+++ src/main/java/com/softartisans/timberwolf/exchange/UserIterator.java
@@ -2,12 +2,13 @@ package com.softartisans.timberwolf.exchange;
 
 import com.softartisans.timberwolf.MailboxItem;
 import java.util.Iterator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-/**
- * An iterator that runs FindFolderIterators for many users, flattening the results.
- */
+/** An iterator that runs FindFolderIterators for many users, flattening the results. */
 public class UserIterator extends BaseChainIterator<MailboxItem>
 {
+    private static final Logger LOG = LoggerFactory.getLogger(UserIterator.class);
     private ExchangeService service;
     private int findItemPageSize;
     private int getItemPageSize;
@@ -25,11 +26,76 @@ public class UserIterator extends BaseChainIterator<MailboxItem>
     @Override
     protected Iterator<MailboxItem> createIterator()
     {
-        if (!users.hasNext())
+        while (users.hasNext())
         {
+            String user = users.next();
+            try
+            {
+                return new SafeIterator(user, new FindFolderIterator(service, findItemPageSize, getItemPageSize, user));
+            }
+            catch (Exception e)
+            {
+                LOG.warn("Failed to find folders for user: {}", user);
+                LOG.debug("Due to exception", e);
+            }
+        }
+        return null;
+    }
+
+    private static class SafeIterator implements Iterator<MailboxItem>
+    {
+
+        private String user;
+        private Iterator<MailboxItem> baseIterator;
+
+        public SafeIterator(String currentUser, Iterator<MailboxItem> iterator)
+        {
+            user = currentUser;
+            baseIterator = iterator;
+        }
+
+        @Override
+        public boolean hasNext()
+        {
+            try
+            {
+                return baseIterator.hasNext();
+            }
+            catch (Exception e)
+            {
+                LOG.warn("Failed to get email for user: {}", user);
+                if (LOG.isDebugEnabled())
+                {
+                    LOG.debug("Due to exception", e);
+                }
+
+            }
+            return false;
+        }
+
+        @Override
+        public MailboxItem next()
+        {
+            try
+            {
+                return baseIterator.next();
+            }
+            catch (Exception e)
+            {
+                LOG.warn("Failed to get email for user: {}", user);
+                if (LOG.isDebugEnabled())
+                {
+                    LOG.debug("Due to exception", e);
+                }
+            }
             return null;
         }
 
-        return new FindFolderIterator(service, findItemPageSize, getItemPageSize, users.next());
+        @Override
+        public void remove()
+        {
+            throw new UnsupportedOperationException();
+        }
     }
+
 }
