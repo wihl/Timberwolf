4064f98dd7fd90269a0f23cc1f4664ba59e509c0
Sean Kermes <seank@ripariandata.com>
2012-01-25 11:38:23 -0500

    Correcting comment

--- src/main/java/com/ripariandata/timberwolf/hbase/HBaseRuntimeException.java
+++ src/main/java/com/ripariandata/timberwolf/hbase/HBaseRuntimeException.java
@@ -2,7 +2,7 @@ package com.ripariandata.timberwolf.hbase;
 
 import org.slf4j.Logger;
 
-/** Non-checked exception thrown by ExchangeMailStore iterator. */
+/** Non-checked exception thrown by classes that talk to HBase. */
 public class HBaseRuntimeException extends RuntimeException
 {
     public HBaseRuntimeException(final String message, final Throwable cause)
