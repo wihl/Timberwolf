8adab6e9607e72e4872d137c3a3d8e6aa06b15df
Scott Dugas <scottd@ripariandata.com>
2012-01-25 11:40:18 -0500

    Sleep between sending emails and calling GetMail

--- src/test/java/com/ripariandata/timberwolf/integrated/TestIntegration.java
+++ src/test/java/com/ripariandata/timberwolf/integrated/TestIntegration.java
@@ -45,6 +45,12 @@ public class TestIntegration
 
     private static final String LDAP_DOMAIN_PROPERTY_NAME = "LdapDomain";
     private static final String LDAP_CONFIG_ENTRY_PROPERTY_NAME = "LdapConfigEntry";
+    /**
+     * The time to sleep between sending emails and calling GetMails.
+     * This is because sometimes we're so fast that we reget the old emails.
+     * This is in milliseconds.
+     */
+    public static final int SLEEP_TIME = 10000;
 
     @Rule
     public IntegrationTestProperties properties = new IntegrationTestProperties(EXCHANGE_URI_PROPERTY_NAME,
@@ -134,7 +140,7 @@ public class TestIntegration
 
     private void runForEmails(final HTableResource emailTable)
             throws ExchangePump.FailedToCreateMessage, ExchangePump.FailedToFindMessage,
-                   ExchangePump.FailedToMoveMessage, LoginException, IOException
+                   ExchangePump.FailedToMoveMessage, LoginException, IOException, InterruptedException
     {
         final ExpectedEmails expectedEmails;
         user1.initialize(pump);
@@ -144,6 +150,9 @@ public class TestIntegration
         user1.sendEmail(pump);
         user2.sendEmail(pump);
         user3.sendEmail(pump);
+        // Sleep a few seconds to make sure all the emails are received
+        // before we continue on to organize and calling GetMail
+        Thread.sleep(SLEEP_TIME);
 
         user1.moveEmails(pump);
         user2.moveEmails(pump);
@@ -264,7 +273,7 @@ public class TestIntegration
     @Test
     public void testIntegrationNoCLI()
             throws PrincipalFetchException, LoginException, IOException, ExchangePump.FailedToCreateMessage,
-                   ExchangePump.FailedToFindMessage, ExchangePump.FailedToMoveMessage
+                   ExchangePump.FailedToFindMessage, ExchangePump.FailedToMoveMessage, InterruptedException
     {
         /*
         This test tests getting emails from an exchange server, and the breadth
