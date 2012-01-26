62441ff0203f415fa61839239e5cf718b42c01c1
Nicholas Martin <nickm@ripariandata.com>
2012-01-26 16:23:35 -0500

    Changed HBaseConfigurator to use our ZK client port constant, as the other one only exists in newer versions of hbase.

--- src/main/java/com/softartisans/timberwolf/hbase/HBaseConfigurator.java
+++ src/main/java/com/softartisans/timberwolf/hbase/HBaseConfigurator.java
@@ -11,18 +11,22 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
 public abstract class HBaseConfigurator
 {
     /**
+     * The property name of the ZooKeeper client port property.
+     */
+     private static final String ZOOKEEPER_CLIENT_PORT = "hbase.zookeeper.property.clientPort";
+
+    /**
      * Creates a Hadoop Configuration for HBase using the specified
      * properties.
      * @param quorum The ZooKeeper quorum members.
      * @param clientPort The ZooKeeper client port.
      * @return A Hadoop Configuration object with the above parameters.
      */
-    public static final Configuration createConfiguration(final String quorum,
-                                                        final String clientPort)
+    public static final Configuration createConfiguration(final String quorum, final String clientPort)
     {
         Configuration configuration = HBaseConfiguration.create();
         configuration.set(HConstants.ZOOKEEPER_QUORUM, quorum);
-        configuration.set(HConstants.ZOOKEEPER_CLIENT_PORT, clientPort);
+        configuration.set(ZOOKEEPER_CLIENT_PORT, clientPort);
         return configuration;
     }
 }
