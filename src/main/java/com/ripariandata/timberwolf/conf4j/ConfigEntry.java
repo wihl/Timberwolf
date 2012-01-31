f21b07bea8c1ae77b0364fbde0af24dbe79d1018
Sean Kermes <seank@ripariandata.com>
2012-01-31 15:51:23 -0500

    Having ConfigEntries stick around for runtime interrogation.

--- src/main/java/com/ripariandata/timberwolf/conf4j/ConfigEntry.java
+++ src/main/java/com/ripariandata/timberwolf/conf4j/ConfigEntry.java
@@ -1,5 +1,12 @@
 package com.ripariandata.timberwolf.conf4j;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD})
 public @interface ConfigEntry
 {
     String name();
