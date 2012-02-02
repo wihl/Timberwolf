3ed71333b6b06f5e2cd4c5562fb304ebb779e3de
Sean Kermes <seank@ripariandata.com>
2012-02-02 13:01:08 -0500

    Adding javadoc.

--- src/main/java/com/ripariandata/timberwolf/conf4j/ConfigEntry.java
+++ src/main/java/com/ripariandata/timberwolf/conf4j/ConfigEntry.java
@@ -30,5 +30,6 @@ import java.lang.annotation.Target;
 @Target({ ElementType.FIELD })
 public @interface ConfigEntry
 {
+    /** The key name in the configuration file for this entry. */
     String name();
 }
