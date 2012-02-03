670865f217446d327eb56c3ea5908c7e8e79a50d
Scott Dugas <scottd@ripariandata.com>
2012-02-03 15:26:27 -0500

    Removed mockFindItem methods

--- src/test/java/com/ripariandata/timberwolf/exchange/ExchangeTestBase.java
+++ src/test/java/com/ripariandata/timberwolf/exchange/ExchangeTestBase.java
@@ -21,9 +21,6 @@ import com.microsoft.schemas.exchange.services.x2006.messages.ArrayOfResponseMes
 import com.microsoft.schemas.exchange.services.x2006.messages.FindFolderResponseMessageType;
 import com.microsoft.schemas.exchange.services.x2006.messages.FindFolderResponseType;
 import com.microsoft.schemas.exchange.services.x2006.messages.FindFolderType;
-import com.microsoft.schemas.exchange.services.x2006.messages.FindItemResponseMessageType;
-import com.microsoft.schemas.exchange.services.x2006.messages.FindItemResponseType;
-import com.microsoft.schemas.exchange.services.x2006.messages.FindItemType;
 import com.microsoft.schemas.exchange.services.x2006.messages.GetItemResponseType;
 import com.microsoft.schemas.exchange.services.x2006.messages.GetItemType;
 import com.microsoft.schemas.exchange.services.x2006.messages.ItemInfoResponseMessageType;
@@ -35,20 +32,19 @@ import com.microsoft.schemas.exchange.services.x2006.types.ArrayOfFoldersType;
 import com.microsoft.schemas.exchange.services.x2006.types.ArrayOfRealItemsType;
 import com.microsoft.schemas.exchange.services.x2006.types.DistinguishedFolderIdNameType;
 import com.microsoft.schemas.exchange.services.x2006.types.FindFolderParentType;
-import com.microsoft.schemas.exchange.services.x2006.types.FindItemParentType;
 import com.microsoft.schemas.exchange.services.x2006.types.FolderIdType;
 import com.microsoft.schemas.exchange.services.x2006.types.FolderType;
 import com.microsoft.schemas.exchange.services.x2006.types.ItemIdType;
 import com.microsoft.schemas.exchange.services.x2006.types.MessageType;
 import com.microsoft.schemas.exchange.services.x2006.types.SyncFolderItemsChangesType;
 import com.microsoft.schemas.exchange.services.x2006.types.SyncFolderItemsCreateOrUpdateType;
-import com.ripariandata.timberwolf.UserTimeUpdater;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 import org.apache.xmlbeans.XmlException;
-import org.joda.time.DateTime;
 import org.junit.Before;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -56,6 +52,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static com.ripariandata.timberwolf.exchange.IsXmlBeansRequest.likeThis;
+
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -118,12 +115,6 @@ public class ExchangeTestBase
         defaultFolder = new FolderContext(DEFAULT_FOLDER_ID, defaultUser);
     }
 
-    protected void mockFindItem(final MessageType[] messages)
-            throws ServiceCallException, HttpErrorException
-    {
-        mockFindItem(messages, DEFAULT_FOLDER_ID, 0, DEFAULT_PAGE_SIZE, defaultUser);
-    }
-
     protected List<String> generateIds(final int offset, final int count, final String folder)
     {
         List<String> ids = new ArrayList<String>(count);
@@ -132,59 +123,6 @@ public class ExchangeTestBase
             ids.add(folder + ":the #" + i + " id");
         }
         return ids;
-    }
-
-    protected MessageType[] mockFindItem(final String folder, final int offset, final int maxIds, final int count)
-            throws ServiceCallException, HttpErrorException
-    {
-        return mockFindItem(folder, offset, maxIds, count, defaultUser);
-    }
-
-    protected MessageType[] mockFindItem(final String folder, final int offset, final int maxIds, final int count,
-                                         final String user) throws ServiceCallException, HttpErrorException
-    {
-        MessageType[] findItems = new MessageType[count];
-        List<String> ids = generateIds(offset, count, folder);
-        for (int i = 0; i < count; i++)
-        {
-            findItems[i] = mockMessageItemId(ids.get(i));
-        }
-        mockFindItem(findItems, folder, offset, maxIds, user);
-        return findItems;
-    }
-
-    private void mockFindItem(final MessageType[] messages, final String folder, final int offset,
-                              final int maxIds, final String user) throws ServiceCallException, HttpErrorException
-    {
-        mockFindItem(messages, folder, offset, maxIds, user, new DateTime(0));
-    }
-
-    protected void mockFindItem(final MessageType[] messages, final String folder, final int offset, final int maxIds,
-                                final String user, final DateTime startDate)
-            throws ServiceCallException, HttpErrorException
-    {
-        Configuration config = new Configuration(maxIds, 0);
-        UserTimeUpdater timeUpdater = mock(UserTimeUpdater.class);
-        when(timeUpdater.lastUpdated(user)).thenReturn(startDate);
-//        config = config.withTimeUpdater(timeUpdater);
-
-        FolderContext folderContext = new FolderContext(folder, user);
-        FindItemType findItem = FindItemHelper.getFindItemsRequest(config, folderContext, offset);
-        FindItemResponseType findItemResponse = mock(FindItemResponseType.class);
-        ArrayOfResponseMessagesType arrayOfResponseMessages = mock(ArrayOfResponseMessagesType.class);
-        FindItemResponseMessageType findItemResponseMessage = mock(FindItemResponseMessageType.class);
-        FindItemParentType findItemParent = mock(FindItemParentType.class);
-        ArrayOfRealItemsType arrayOfRealItems = mock(ArrayOfRealItemsType.class);
-        when(service.findItem(likeThis(findItem), eq(user))).thenReturn(findItemResponse);
-        when(findItemResponse.getResponseMessages()).thenReturn(arrayOfResponseMessages);
-        when(arrayOfResponseMessages.getFindItemResponseMessageArray())
-                .thenReturn(new FindItemResponseMessageType[]{findItemResponseMessage});
-        when(findItemResponseMessage.getResponseCode()).thenReturn(ResponseCodeType.NO_ERROR);
-        when(findItemResponseMessage.isSetRootFolder()).thenReturn(true);
-        when(findItemResponseMessage.getRootFolder()).thenReturn(findItemParent);
-        when(findItemParent.isSetItems()).thenReturn(true);
-        when(findItemParent.getItems()).thenReturn(arrayOfRealItems);
-        when(arrayOfRealItems.getMessageArray()).thenReturn(messages);
     }
 
     protected FolderContext folderContext(final String user, final String folderId)
