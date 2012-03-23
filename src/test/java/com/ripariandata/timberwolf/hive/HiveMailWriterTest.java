58b49e31c74563e41a0acd4b2d3a16e4e910f481
Sean Kermes <seank@ripariandata.com>
2012-03-23 14:47:34 -0400

    Testing new table path.

--- src/test/java/com/ripariandata/timberwolf/hive/HiveMailWriterTest.java
+++ src/test/java/com/ripariandata/timberwolf/hive/HiveMailWriterTest.java
@@ -20,6 +20,7 @@ package com.ripariandata.timberwolf.hive;
 import com.ripariandata.timberwolf.MailWriter;
 import com.ripariandata.timberwolf.MailboxItem;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 import java.net.URI;
@@ -51,9 +52,15 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.startsWith;
+import static org.mockito.Matchers.matches;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.regex.PatternSyntaxException;
 
 public class HiveMailWriterTest
 {
@@ -68,5 +75,40 @@ public class HiveMailWriterTest
 
         verify(hive).close();
         verify(hdfs).close();
+    }
+
+    @Test
+    @SuppressWarnings("deprecation")
+    public void testWritingToNewTable() throws SQLException, IOException
+    {
+        Connection hive = mock(Connection.class);
+        FileSystem hdfs = mock(FileSystem.class);
+
+        PreparedStatement showStmt = mock(PreparedStatement.class);
+        ResultSet showResult = mock(ResultSet.class);
+        when(showResult.next()).thenReturn(false);
+        when(showStmt.executeQuery()).thenReturn(showResult);
+        when(hive.prepareStatement("show tables ?")).thenReturn(showStmt);
+
+        Statement createStmt = mock(Statement.class);
+        when(hive.createStatement()).thenReturn(createStmt);
+
+        PreparedStatement loadStmt = mock(PreparedStatement.class);
+        when(hive.prepareStatement("load data inpath ? into table new_table")).thenReturn(loadStmt);
+
+        when(hdfs.exists(eq(new Path("/tmp/timberwolf")))).thenReturn(false);
+        when(hdfs.create(any(Path.class))).thenReturn(new FSDataOutputStream(new ByteArrayOutputStream()));
+
+        HiveMailWriter writer = new HiveMailWriter(hdfs, hive, "new_table");
+        writer.write(new ArrayList<MailboxItem>());
+
+        verify(showStmt).setString(1, "new_table");
+        String s = "create table new_table \\([^)]+\\) row format delimited fields terminated by '\\\\037' stored as sequencefile";
+        verify(createStmt).executeQuery(matches(s));
+        verify(loadStmt).setString(eq(1), startsWith("/tmp/timberwolf/"));
+        verify(loadStmt).executeQuery();
+
+        verify(hdfs).mkdirs(eq(new Path("/tmp/timberwolf")));
+        verify(hdfs).delete(any(Path.class), eq(false));
     }
 }
